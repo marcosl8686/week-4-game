0e974014d3274d584358695a07fb28ba70bdfe0a
Marcos Lee <Marcos@Marcoss-MacBook-Pro.local>
2017-02-23 18:56:15 -0800

    add better animation, and end game rules to game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,24 +4,22 @@ var healer = {
 	mp: 200,
 	heal: 50,
 	attack: 1,
-	armor: 0,
 }
 var warrior = {
 	hp: 450,
-	attack: 20,
-	armor: 10,
+	attack: 600,
+	rage: 0,
 }
 var mage = {
 	hp: 200,
 	mp: 200,
 	fire: 50,
 	attack: 1,
-	armor: 0,
 }
 var enemy = {
 	hp: 600,
 	attack:100,
-	armor:10,
+	special: 100,
 }
 var delay = 1000;
 // JavaScript function that wraps everything
@@ -53,27 +51,32 @@ $(document).ready(function() {
 	}	
 	function mageSpeedReturn() {
 		$("#mageSpeed").removeAttr("class")
+	}
+	function fireSpell() {
+		$("#fireSpell").removeAttr("src")
+	}
+	function explosion() {
+		$("#explosion").removeAttr("src")
+	}
+	function skeleton() {
+		$("#enemy").attr("src", "assets/images/skeleton.png")
 	}		
 
 	//link hp, MP , armor to HTML
 	$("#healerHpBar").html(healer.hp);
 	$("#healerMpBar").html(healer.mp);
-	$("#healerArmor").html(healer.armor);
 	$("#warriorHpBar").html(warrior.hp);		
-	$("#warriorArmor").html(warrior.armor);
+	$("#warriorArmor").html(warrior.rage);
 	$("#mageHpBar").html(mage.hp);
 	$("#mageMpBar").html(mage.mp);
-	$("#mageArmor").html(mage.armor);
 	$("#enemyHpBar").html(enemy.hp);
 	function refresh() {
 		$("#healerHpBar").html(healer.hp);
 		$("#healerMpBar").html(healer.mp);
-		$("#healerArmor").html(healer.armor);
 		$("#warriorHpBar").html(warrior.hp);
-		$("#warriorArmor").html(warrior.armor);
+		$("#warriorArmor").html(warrior.rage);
 		$("#mageHpBar").html(mage.hp);
 		$("#mageMpBar").html(mage.mp);
-		$("#mageArmor").html(mage.armor);
 		$("#enemyHpBar").html(enemy.hp);
 	}
 	//Healer command starts here
@@ -109,10 +112,8 @@ $(document).ready(function() {
 			refresh();
 		}
 		else if ($(this).hasClass("healer-guard")) {
-			healer.armor += 2;
 			healer.mp += 25;
 			$("#healerSpeed").toggleClass("healerSpeed");
-			console.log(healer.armor + " healer armor");
 			console.log(healer.mp + " healer mp");
 			$(".healerAction").attr("disabled","disabled");
 			setTimeout(healerButton, 10000);
@@ -128,17 +129,24 @@ $(document).ready(function() {
 			$(".warriorAction").attr("disabled","disabled");
 			$("#warrior").attr("src","assets/images/warrior.attack.gif")
 			enemy.hp -= warrior.attack;
+			warrior.rage += 15;
 			console.log(enemy.hp);
 			setTimeout(warriorReturn, 1000);
 			setTimeout(warriorButton, 7000);
 			setTimeout(warriorSpeedReturn, 7000);
 			refresh();
+			if (enemy.hp <= 0) {
+			$("#enemy").attr("src","assets/images/fire1.gif")
+			setTimeout(skeleton, 3000);
+			}
 		}
 		else if ($(this).hasClass("warrior-guard")) {
 			$("#warriorSpeed").toggleClass("warriorSpeed");
 			$(".warriorAction").attr("disabled","disabled");
-			warrior.armor += 2;
-			console.log(warrior.armor + " warrior armor");
+			$("#warrior").attr("src","assets/images/warrior.attack.gif")
+			enemy.hp -= warrior.rage
+			warrior.rage = 0;
+			setTimeout(warriorReturn, 1000);
 			setTimeout(warriorButton, 7000);
 			setTimeout(warriorSpeedReturn, 7000);
 			refresh();
@@ -168,18 +176,18 @@ $(document).ready(function() {
 				console.log(enemy.hp + " enemy hp")
 				console.log(mage.mp + " mage mp")
 				$("#mage").attr("src","assets/images/mage.cast.gif")
+				$("#fireSpell").attr("src","assets/images/fire.spell.gif")
 				setTimeout(mageReturn, 1500);
 				refresh();
 				setTimeout(mageButton, 8000);
 				setTimeout(mageSpeedReturn, 8000);
+				setTimeout(fireSpell, 1500);
 			}
 		}
 		else if ($(this).hasClass("mage-guard")) {
 			$("#mageSpeed").toggleClass("mageSpeed");
 			$(".mageAction").attr("disabled","disabled");
-			mage.armor += 2;
 			mage.mp += 25;
-			console.log(mage.armor + " mage armor");
 			console.log(mage.mp + " mage mp")
 			refresh();
 			setTimeout(mageButton, 8000);
@@ -228,7 +236,27 @@ $(document).ready(function() {
 			}
 		}	
 	}
+	function specialAttack() {	
+		mage.hp -= enemy.special;
+		warrior.hp -= enemy.special;
+		healer.hp -= enemy.special;
+		$("#explosion").attr("src","assets/images/explosion.gif")
+		setTimeout(explosion, 2000);
+		if (warrior.hp <= 0) {
+			$(".warriorAction").attr("disabled","disabled");
+			$("#warrior").attr("src","assets/images/stone.png");
+		}
+		else if (healer.hp <= 0) {
+			$(".healerAction").attr("disabled","disabled");
+			$("#healer").attr("src","assets/images/stone.png");
+		}
+		else if (mage.hp <= 0) {
+			$(".mageAction").attr("disabled","disabled");
+			$("#mage").attr("src","assets/images/stone.png");
+		}
+	}
 	setInterval(enemyAttack, 8000);
+	setInterval(specialAttack, 30000);
 	//enemy command ends
 
 	//Game victory or lost criteria
